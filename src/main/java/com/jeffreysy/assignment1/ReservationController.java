f04dbcdaa16390804a4a0d092f843daf9e9791b7
jeffjsy <jeffjsy@gmail.com>
2023-06-02 18:04:48 -0400

    2nd commit

--- src/main/java/com/jeffreysy/assignment1/ReservationController.java
+++ src/main/java/com/jeffreysy/assignment1/ReservationController.java
@@ -23,39 +23,36 @@ public class ReservationController {
 
 	@PostMapping("/show-details")
 	public String processForm(@Valid Reservation reservation, BindingResult bindingResult, Model model) {
+		LocalDate checkInDate = reservation.getCheckInDate();
+	    LocalDate checkOutDate = reservation.getCheckOutDate();
+	    LocalDate currentDate = LocalDate.now();
 
-		if (bindingResult.hasErrors()) { // Prevent the user from moving past form if any data is invalid  
-			return "index";
+	    if (bindingResult.hasErrors() || checkInDate.isBefore(currentDate) || checkOutDate.isBefore(currentDate) || checkOutDate.isBefore(checkInDate)) {
+	        if (checkInDate.isBefore(currentDate) || checkOutDate.isBefore(currentDate) || checkOutDate.isBefore(checkInDate)) {
+	            model.addAttribute("message", "Invalid dates. Please ensure the dates are valid");
+	        } else {
+	            model.addAttribute("message", "Please correct the errors in the form.");
+	        }
+	        model.addAttribute("reservation", reservation);
+	        return "error";
 		} else { 
-			LocalDate checkInDate = reservation.getCheckInDate();
-			LocalDate checkOutDate = reservation.getCheckOutDate();
-			String viewType = reservation.getViewType();
-
-			if (!reservation.validateDates(checkInDate, checkOutDate)) {
-				// Dates are invalid, handle the error
-				model.addAttribute("error", "Invalid dates");
-				return "index";
-			}
-
-			double totalCost = reservation.CalculateTotalCost(checkInDate, checkOutDate, viewType);
-
+			double totalCost = reservation.CalculateTotalCost(checkInDate, checkOutDate, reservation.getViewType());
 
 			// Output details of reservation
 			String output = "Welcome " + reservation.getFname() + "<br><br>" +
 					"Name: " + reservation.getFname() + " " + reservation.getLname() + "<br>" +
 					"Phone: " + reservation.getPhone() + "<br>" +
 					"Email: " + reservation.getEmail() + "<br>" +
-					"Address: " + reservation.getAddress() + "<br>" +
-					"City: " + reservation.getCity() + "<br>" +
-					"Postal Code: " + reservation.getPostal() + "<br>" +
-					"Province: " + reservation.getProvince() + "<br><br>" +
+					"Address: " + reservation.getAddress() + ", " +					
+					reservation.getCity() + ", " +
+					reservation.getPostal() + ", " +
+					reservation.getProvince() + "<br><br>" +
 					"Accommodations:<br>" +
 					"Check-in Date: " + reservation.getCheckInDate() + "<br>" +
 					"Check-out Date: " + reservation.getCheckOutDate() + "<br>" +
 					"Number of Nights: " + reservation.CalculateNumNights(checkInDate, checkOutDate) + "<br>" +
 					"Room Type: " + reservation.getViewType() + "<br>" +					 
-					"Number of Adults: " + reservation.getNumAdults() + "<br>" +
-					"Number of Children: " + reservation.getNumChildren() + "<br><br>" +
+					"Guests: " + reservation.getNumAdults() + " adults, "+ reservation.getNumChildren() + " children" + "<br><br>" +
 					"Payment Method: " + reservation.getPaymentType() + "<br><br><br>" +
 					"Total Cost: " + totalCost;
 
